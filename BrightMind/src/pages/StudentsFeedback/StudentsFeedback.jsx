0d1977ff1949714b8a065559988649ee1d68d9d7
mirazahmeed <mirazahmed0619@gmail.com>
2025-07-18 01:40:12 +0600

    page setup for BrightMind project components and pages added

--- BrightMind/src/pages/StudentsFeedback/StudentsFeedback.jsx
+++ BrightMind/src/pages/StudentsFeedback/StudentsFeedback.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => {
+const StudentsFeedback = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const App = () => {
   )
 }
 
-export default App
+export default StudentsFeedback
